0e43bf90167bd4a4d0287f1f342f34cd78743bbe
Gloryadeola <gloryadeola09@gmail.com>
2023-06-17 21:13:08 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - A program that prints the size of various computer types
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwisie
  */
 int main(void)
 {
