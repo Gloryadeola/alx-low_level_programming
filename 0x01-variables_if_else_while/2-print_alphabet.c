492d38e1dc0cd75c7a406cb044b73fb5fa61207b
Gloryadeola <gloryadeola09@gmail.com>
2023-06-19 20:47:48 +0000

    print-alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - A program that prints alphabets in lowercase.
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putcha('\n');
+	return (0);
+}
