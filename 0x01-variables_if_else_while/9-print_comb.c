12aa60fb9eccd57dcaacda67842a30b0be4f8ff5
Gloryadeola <gloryadeola09@gmail.com>
2023-06-20 02:46:59 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possibel combination of single-digit numbers
+ * printed in ascending order
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for ( n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
