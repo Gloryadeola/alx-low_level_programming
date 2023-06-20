761ef8130c4825ce93532c1acd0f13e11c86ac62
Gloryadeola <gloryadeola09@gmail.com>
2023-06-20 01:34:35 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print all single didgits of base 10 starting from 0
+ * followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++
+	}
+	printf('\n');
+	return (0);
+}
